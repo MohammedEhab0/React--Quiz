bf61c581c04142d32ece56ae12254bb865ccdf8b
mohammed-Ehab <116813776+MohammedEhab0@users.noreply.github.com>
2024-03-16 01:12:45 +0200

    Delete src directory

--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import logoimg from "../assets/quiz-logo.png";
-export default function Header() {
-  return (
-    <header>
-      <img src={logoimg} alt="quiz logo" />
-      <h1>ReactQuiz</h1>
-    </header>
-  );
-}
